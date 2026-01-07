bed3e4b9e0aa0446c80d502506f6d9c99363ecd8
Praise Jacob <iampraisejacob@gmail.com>
2026-01-07 09:00:49 +0530

    refactor: split code gen macro for each function

--- include/__internals/__string_gen_macro.h
+++ include/__internals/__string_gen_macro.h
@@ -6,45 +6,66 @@
 #include "hashmap_string.h" // IWYU pragma: keep
 #include <stdlib.h>         // IWYU pragma: keep
 //
-#define HASHMAP_VALUE_REF(type, value) (&(type) { value })
-
+#define HASHMAP_VALUE_REF(type, value)  (&(type) { value })
 #define HASHMAP_TYPE_STRING(value_name) hashmap_string_##value_name##_t
-#define HASHMAP_DEFINE_STRING_TYPE(value_type, value_name)                                                             \
-    typedef struct HASHMAP_TYPE_STRING(value_type) {                                                                   \
-        hashmap_t* __internal;                                                                                         \
-    } HASHMAP_TYPE_STRING(value_type);                                                                                 \
+
+#define HASHMAP_CREATE_STRING(value_type, value_name)                                                                  \
     static inline HASHMAP_TYPE_STRING(value_type) * hashmap_create_string_##value_name(void)                           \
     {                                                                                                                  \
         HASHMAP_TYPE_STRING(value_type)* wrapper = malloc(sizeof(HASHMAP_TYPE_STRING(value_type)));                    \
         wrapper->__internal                      = (hashmap_t*)hashmap_create_string(sizeof(value_type));              \
         return wrapper;                                                                                                \
-    }                                                                                                                  \
+    }
+
+#define HASHMAP_SET_STRING(value_type, value_name)                                                                     \
     static inline int hashmap_set_string_##value_name(                                                                 \
         HASHMAP_TYPE_STRING(value_type) * map, char* key, value_type value)                                            \
     {                                                                                                                  \
         return hashmap_set(map->__internal, key, HASHMAP_VALUE_REF(value_type, value));                                \
-    }                                                                                                                  \
+    }
+
+#define HASHMAP_GET_STRING(value_type, value_name)                                                                     \
     static inline value_type hashmap_get_string_##value_name(HASHMAP_TYPE_STRING(value_type) * map, char* key)         \
     {                                                                                                                  \
         value_type* value_ptr = (value_type*)hashmap_get(map->__internal, key);                                        \
         return value_ptr ? *value_ptr : (value_type) { 0 };                                                            \
-    }                                                                                                                  \
+    }
+
+#define HASHMAP_GETP_STRING(value_type, value_name)                                                                    \
     static inline value_type* hashmap_getp_string_##value_name(HASHMAP_TYPE_STRING(value_type) * map, char* key)       \
     {                                                                                                                  \
         return (value_type*)hashmap_get(map->__internal, key);                                                         \
-    }                                                                                                                  \
+    }
+
+#define HASHMAP_HAS_STRING(value_type, value_name)                                                                     \
     static inline int hashmap_has_string_##value_name(HASHMAP_TYPE_STRING(value_type) * map, char* key)                \
     {                                                                                                                  \
         return hashmap_has(map->__internal, key);                                                                      \
-    }                                                                                                                  \
+    }
+
+#define HASHMAP_REMOVE_STRING(value_type, value_name)                                                                  \
     static inline int hashmap_remove_string_##value_name(HASHMAP_TYPE_STRING(value_type) * map, char* key)             \
     {                                                                                                                  \
         return hashmap_remove(map->__internal, key);                                                                   \
-    }                                                                                                                  \
+    }
+
+#define HASHMAP_DESTROY_STRING(value_type, value_name)                                                                 \
     static inline void hashmap_destroy_string_##value_name(HASHMAP_TYPE_STRING(value_type) * map)                      \
     {                                                                                                                  \
         hashmap_destroy(map->__internal);                                                                              \
         free(map);                                                                                                     \
     }
+
+#define HASHMAP_DEFINE_STRING_TYPE(value_type, value_name)                                                             \
+    typedef struct HASHMAP_TYPE_STRING(value_type) {                                                                   \
+        hashmap_t* __internal;                                                                                         \
+    } HASHMAP_TYPE_STRING(value_type);                                                                                 \
+    HASHMAP_CREATE_STRING(value_type, value_name)                                                                      \
+    HASHMAP_SET_STRING(value_type, value_name)                                                                         \
+    HASHMAP_GET_STRING(value_type, value_name)                                                                         \
+    HASHMAP_GETP_STRING(value_type, value_name)                                                                        \
+    HASHMAP_HAS_STRING(value_type, value_name)                                                                         \
+    HASHMAP_REMOVE_STRING(value_type, value_name)                                                                      \
+    HASHMAP_DESTROY_STRING(value_type, value_name)
 
 #endif // __FELIX_HASHMAP_HELPERS_STRING_H__
--- include/__internals/__type_gen_macro.h
+++ include/__internals/__type_gen_macro.h
@@ -5,48 +5,86 @@
 #include "hashmap.h" // IWYU pragma: keep
 #include <stdlib.h>  // IWYU pragma: keep
 
-#define HASHMAP_KEY_REF(type, value)   (&(type) { value })
-#define HASHMAP_VALUE_REF(type, value) (&(type) { value })
-
+#define HASHMAP_KEY_REF(type, value)       (&(type) { value })
+#define HASHMAP_VALUE_REF(type, value)     (&(type) { value })
 #define HASHMAP_TYPE(key_name, value_name) hashmap_##key_name##_##value_name##_t
+
+#define HASHMAP_CREATE(key_type, key_name, value_type, value_name)                                                     \
+    static inline HASHMAP_TYPE(key_name, value_name) * hashmap_create_##key_name##_##value_name(void)                  \
+    {                                                                                                                  \
+        HASHMAP_TYPE(key_name, value_name)* wrapper = malloc(sizeof(HASHMAP_TYPE(key_name, value_name)));              \
+        if (!wrapper)                                                                                                  \
+            return NULL;                                                                                               \
+        wrapper->__internal = (hashmap_t*)hashmap_create_default(sizeof(key_type), sizeof(value_type));                \
+        if (!wrapper->__internal) {                                                                                    \
+            free(wrapper);                                                                                             \
+            return NULL;                                                                                               \
+        }                                                                                                              \
+        return wrapper;                                                                                                \
+    }
+
+#define HASHMAP_SET(key_type, key_name, value_type, value_name)                                                        \
+    static inline int hashmap_set_##key_name##_##value_name(                                                           \
+        HASHMAP_TYPE(key_name, value_name) * map, key_type key, value_type value)                                      \
+    {                                                                                                                  \
+        if (!map || !map->__internal)                                                                                  \
+            return -1;                                                                                                 \
+        return hashmap_set(map->__internal, HASHMAP_KEY_REF(key_type, key), HASHMAP_VALUE_REF(value_type, value));     \
+    }
+
+#define HASHMAP_GET(key_type, key_name, value_type, value_name)                                                        \
+    static inline value_type hashmap_get_##key_name##_##value_name(                                                    \
+        HASHMAP_TYPE(key_name, value_name) * map, key_type key)                                                        \
+    {                                                                                                                  \
+        if (!map || !map->__internal)                                                                                  \
+            return (value_type) { 0 };                                                                                 \
+        value_type* value_ptr = (value_type*)hashmap_get(map->__internal, HASHMAP_KEY_REF(key_type, key));             \
+        return value_ptr ? *value_ptr : (value_type) { 0 };                                                            \
+    }
+
+#define HASHMAP_GETP(key_type, key_name, value_type, value_name)                                                       \
+    static inline value_type* hashmap_getp_##key_name##_##value_name(                                                  \
+        HASHMAP_TYPE(key_name, value_name) * map, key_type key)                                                        \
+    {                                                                                                                  \
+        if (!map || !map->__internal)                                                                                  \
+            return NULL;                                                                                               \
+        return (value_type*)hashmap_get(map->__internal, HASHMAP_KEY_REF(key_type, key));                              \
+    }
+
+#define HASHMAP_HAS(key_type, key_name, value_type, value_name)                                                        \
+    static inline int hashmap_has_##key_name##_##value_name(HASHMAP_TYPE(key_name, value_name) * map, key_type key)    \
+    {                                                                                                                  \
+        if (!map || !map->__internal)                                                                                  \
+            return -1;                                                                                                 \
+        return hashmap_has(map->__internal, HASHMAP_KEY_REF(key_type, key));                                           \
+    }
+
+#define HASHMAP_REMOVE(key_type, key_name, value_type, value_name)                                                     \
+    static inline int hashmap_remove_##key_name##_##value_name(HASHMAP_TYPE(key_name, value_name) * map, key_type key) \
+    {                                                                                                                  \
+        if (!map || !map->__internal)                                                                                  \
+            return -1;                                                                                                 \
+        return hashmap_remove(map->__internal, HASHMAP_KEY_REF(key_type, key));                                        \
+    }
+
+#define HASHMAP_DESTROY(key_type, key_name, value_type, value_name)                                                    \
+    static inline void hashmap_destroy_##key_name##_##value_name(HASHMAP_TYPE(key_name, value_name) * map)             \
+    {                                                                                                                  \
+        hashmap_destroy(map->__internal);                                                                              \
+        if (map)                                                                                                       \
+            free(map);                                                                                                 \
+    }
+
 #define HASHMAP_DEFINE_TYPE(key_type, key_name, value_type, value_name)                                                \
     typedef struct HASHMAP_TYPE(key_name, value_name) {                                                                \
         hashmap_t* __internal;                                                                                         \
     } HASHMAP_TYPE(key_name, value_name);                                                                              \
-    static inline HASHMAP_TYPE(key_name, value_name) * hashmap_create_##key_name##_##value_name(void)                  \
-    {                                                                                                                  \
-        HASHMAP_TYPE(key_name, value_name)* wrapper = malloc(sizeof(HASHMAP_TYPE(key_name, value_name)));              \
-        wrapper->__internal = (hashmap_t*)hashmap_create_default(sizeof(key_type), sizeof(value_type));                \
-        return wrapper;                                                                                                \
-    }                                                                                                                  \
-    static inline int hashmap_set_##key_name##_##value_name(                                                           \
-        HASHMAP_TYPE(key_name, value_name) * map, key_type key, value_type value)                                      \
-    {                                                                                                                  \
-        return hashmap_set(map->__internal, HASHMAP_KEY_REF(key_type, key), HASHMAP_VALUE_REF(value_type, value));     \
-    }                                                                                                                  \
-    static inline value_type hashmap_get_##key_name##_##value_name(                                                    \
-        HASHMAP_TYPE(key_name, value_name) * map, key_type key)                                                        \
-    {                                                                                                                  \
-        value_type* value_ptr = (value_type*)hashmap_get(map->__internal, HASHMAP_KEY_REF(key_type, key));             \
-        return value_ptr ? *value_ptr : (value_type) { 0 };                                                            \
-    }                                                                                                                  \
-    static inline value_type* hashmap_getp_##key_name##_##value_name(                                                  \
-        HASHMAP_TYPE(key_name, value_name) * map, key_type key)                                                        \
-    {                                                                                                                  \
-        return (value_type*)hashmap_get(map->__internal, HASHMAP_KEY_REF(key_type, key));                              \
-    }                                                                                                                  \
-    static inline int hashmap_has_##key_name##_##value_name(HASHMAP_TYPE(key_name, value_name) * map, key_type key)    \
-    {                                                                                                                  \
-        return hashmap_has(map->__internal, HASHMAP_KEY_REF(key_type, key));                                           \
-    }                                                                                                                  \
-    static inline int hashmap_remove_##key_name##_##value_name(HASHMAP_TYPE(key_name, value_name) * map, key_type key) \
-    {                                                                                                                  \
-        return hashmap_remove(map->__internal, HASHMAP_KEY_REF(key_type, key));                                        \
-    }                                                                                                                  \
-    static inline void hashmap_destroy_##key_name##_##value_name(HASHMAP_TYPE(key_name, value_name) * map)             \
-    {                                                                                                                  \
-        hashmap_destroy(map->__internal);                                                                              \
-        free(map);                                                                                                     \
-    }
+    HASHMAP_CREATE(key_type, key_name, value_type, value_name)                                                         \
+    HASHMAP_SET(key_type, key_name, value_type, value_name)                                                            \
+    HASHMAP_GET(key_type, key_name, value_type, value_name)                                                            \
+    HASHMAP_GETP(key_type, key_name, value_type, value_name)                                                           \
+    HASHMAP_HAS(key_type, key_name, value_type, value_name)                                                            \
+    HASHMAP_REMOVE(key_type, key_name, value_type, value_name)                                                         \
+    HASHMAP_DESTROY(key_type, key_name, value_type, value_name)
 
 #endif
